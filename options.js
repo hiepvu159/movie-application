35fef523b65cd4ef1758ee2e9639a35aa1a1d227
Hiep Vu <vhhiep1509@gmail.com>
2022-09-29 07:38:54 +0700

    1

---
+++ options.js
@@ -0,0 +1,38 @@
+export const options = [
+  {
+    value: "Hành Động",
+    label: "Hành Động",
+  },
+  {
+    value: "Tâm Lý",
+    label: "Tâm Lý",
+  },
+  {
+    value: "Tình Cảm",
+    label: "Tình Cảm",
+  },
+  {
+    value: "Kinh Dị",
+    label: "Kinh Dị",
+  },
+  {
+    value: "Viễn Tưởng",
+    label: "Viễn Tưởng",
+  },
+  {
+    value: "Phiêu Lưu",
+    label: "Phiêu Lưu",
+  },
+  {
+    value: "Gia Đình",
+    label: "Gia Đình",
+  },
+  {
+    value: "Hài Hước",
+    label: "Hài Hước",
+  },
+  {
+    value: "Bí Ẩn",
+    label: "Bí Ẩn",
+  },
+];
